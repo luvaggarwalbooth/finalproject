cc790938d1e6e5fefe725335e4ab2a47b5736f42
luvaggarwalbooth <aggarwal.luv@chicagobooth.edu>
2022-12-09 15:38:18 +0000

    final

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -10,4 +10,16 @@ def listing
   render({ :template => "listings.html" })
 end
 
+def new_registration_form
+    
+  render({ :template => "users/sign_up_form.html.erb" })
+
+end
+
+def new_session_form
+  
+  render({ :template => "/signin_form.html.erb" })
+
+end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -3,5 +3,8 @@ Rails.application.routes.draw do
   get("/", {:controller => "application", :action => "home" })
   get("/properties", {:controller => "application", :action => "listing" })
 
+  get("/user_sign_up", {:controller => "application", :action => "new_registration_form" })
+  get("/user_sign_out", {:controller => "application", :action => "toast_cookies" })
+  get("/user_sign_in", {:controller => "application", :action => "new_session_form" })
 
 end
