986f5f526fc473f69ee898c91003235c53a5094e
luvaggarwalbooth <aggarwal.luv@chicagobooth.edu>
2022-12-09 15:15:56 +0000

    final

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,13 @@
 class ApplicationController < ActionController::Base
+
+def home
+
+  render({ :template => "index.html" })
+end
+
+def listing
+
+  render({ :template => "listings.html" })
+end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
 
+  get("/", {:controller => "application", :action => "home" })
+  get("/properties", {:controller => "application", :action => "listing" })
 
 
 end
